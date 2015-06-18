4cbf8d5b4c6155d30f54cc784a7fa84d3e2a190a
tkmagesh <tkmagesh77@gmail.com>
2015-06-18 10:12:50 +0530

    Web Sockets - Chat App

---
+++ Day-02/04-WebSockets/server/app.js
@@ -0,0 +1,13 @@
+
+var nodejsWebSocket = require('nodejs-websocket');
+var server = nodejsWebSocket.createServer(function(connection){
+    console.log("A new client connected");
+    connection.on("text", function(msg){
+        for(var i=0; i<server.connections.length; i++){
+             var con = server.connections[i];
+            con.sendText(msg);
+        }
+    })
+});
+server.listen(9999);
+console.log("Socket server listening on port 9999");
